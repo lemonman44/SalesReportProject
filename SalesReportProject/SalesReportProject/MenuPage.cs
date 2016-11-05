cfa01aa482541b40ccc188b52ffd3dad0ba9b8b1
lemonman44 <AdamLehman2018@gmail.com>
2016-11-05 15:14:19 -0500

    Made 3 forms, MenuPage, PreviewDataAndSendPage, and SettingsPage

---
+++ SalesReportProject/SalesReportProject/MenuPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalesReportProject
+{
+    public partial class MenuPage : Form
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- SalesReportProject/SalesReportProject/SettingsPage.cs
+++ SalesReportProject/SalesReportProject/SettingsPage.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace SalesReportProject
 {
-    public partial class Form1 : Form
+    public partial class SettingsPage : Form
     {
-        public Form1()
+        public SettingsPage()
         {
             InitializeComponent();
         }
