f747adf667842f45aa9fad51ee2454f1f07e4727
lemonman44 <AdamLehman2018@gmail.com>
2016-11-05 17:30:00 -0500

    yeah

--- SalesReportProject/SalesReportProject/MainWindow.Designer.cs
+++ SalesReportProject/SalesReportProject/MainWindow.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuPage = new System.Windows.Forms.Panel();
+            this.menuToSettingsButton = new System.Windows.Forms.Button();
+            this.menuToPreviewButton = new System.Windows.Forms.Button();
+            this.previewAndSendDataPage = new System.Windows.Forms.Panel();
+            this.settingsPage = new System.Windows.Forms.Panel();
+            this.menuPage.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuPage
+            // 
+            this.menuPage.Controls.Add(this.menuToSettingsButton);
+            this.menuPage.Controls.Add(this.menuToPreviewButton);
+            this.menuPage.Location = new System.Drawing.Point(0, 0);
+            this.menuPage.Name = "menuPage";
+            this.menuPage.Size = new System.Drawing.Size(1473, 929);
+            this.menuPage.TabIndex = 0;
+            // 
+            // menuToSettingsButton
+            // 
+            this.menuToSettingsButton.Location = new System.Drawing.Point(1402, 12);
+            this.menuToSettingsButton.Name = "menuToSettingsButton";
+            this.menuToSettingsButton.Size = new System.Drawing.Size(60, 60);
+            this.menuToSettingsButton.TabIndex = 1;
+            this.menuToSettingsButton.UseVisualStyleBackColor = true;
+            this.menuToSettingsButton.Click += new System.EventHandler(this.menuToSettingsButton_Click);
+            // 
+            // menuToPreviewButton
+            // 
+            this.menuToPreviewButton.Location = new System.Drawing.Point(636, 551);
+            this.menuToPreviewButton.Name = "menuToPreviewButton";
+            this.menuToPreviewButton.Size = new System.Drawing.Size(200, 100);
+            this.menuToPreviewButton.TabIndex = 0;
+            this.menuToPreviewButton.Text = "Click Here";
+            this.menuToPreviewButton.UseVisualStyleBackColor = true;
+            this.menuToPreviewButton.Click += new System.EventHandler(this.menuToPreviewButton_Click);
+            // 
+            // previewAndSendDataPage
+            // 
+            this.previewAndSendDataPage.Location = new System.Drawing.Point(0, 0);
+            this.previewAndSendDataPage.Name = "previewAndSendDataPage";
+            this.previewAndSendDataPage.Size = new System.Drawing.Size(1473, 929);
+            this.previewAndSendDataPage.TabIndex = 0;
+            // 
+            // settingsPage
+            // 
+            this.settingsPage.Location = new System.Drawing.Point(0, 0);
+            this.settingsPage.Name = "settingsPage";
+            this.settingsPage.Size = new System.Drawing.Size(1473, 929);
+            this.settingsPage.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1474, 929);
+            this.Controls.Add(this.menuPage);
+            this.Controls.Add(this.previewAndSendDataPage);
+            this.Controls.Add(this.settingsPage);
+            this.Name = "MainWindow";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.SizeChanged += new System.EventHandler(this.MainWindow_SizeChanged);
+            this.menuPage.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel menuPage;
+        private System.Windows.Forms.Panel previewAndSendDataPage;
+        private System.Windows.Forms.Panel settingsPage;
+        private System.Windows.Forms.Button menuToSettingsButton;
+        private System.Windows.Forms.Button menuToPreviewButton;
     }
 }
 
--- SalesReportProject/SalesReportProject/MainWindow.cs
+++ SalesReportProject/SalesReportProject/MainWindow.cs
@@ -12,9 +12,74 @@ namespace SalesReportProject
 {
     public partial class MainWindow : Form
     {
+        //The folling is a description of our layout.
+
+        //We have one main form called MainWindow
+        //We have three (as of right now) panels that are shown on the MainWindow
+        //Only one of these panels is visible at a time on the MainWindow
+        //Each panel has its own groupings of controls on each.
+        //The menuPage panel has a "Click Here" button and a settings button
+        //Each of those buttons causes the menuPage panel to become invisible and another panel to become visible
+        //etc. etc. you probably get the gist
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        //this section is some code that will run before MainWindow is visible to the user
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            //the following code decides which panel is visible on startup and which panels are invisible
+            menuPage.Visible = true;
+            previewAndSendDataPage.Visible = false;
+            settingsPage.Visible = false;
+
+            //the following code sets all the panels to be the same size as the form
+            //ClientSize is the size of the form MainWindow
+            menuPage.Size = new Size(ClientSize.Width, ClientSize.Height);
+            previewAndSendDataPage.Size = new Size(ClientSize.Width, ClientSize.Height);
+            settingsPage.Size = new Size(ClientSize.Width, ClientSize.Height);
+
+            //the following code sets the locations of buttons and other controls to be more centered
+            //and looking like they're in thought out locations
+            menuToPreviewButton.Location
+                = new Point(ClientSize.Width / 2 - menuToPreviewButton.Width / 2, ClientSize.Height / 2);
+            menuToSettingsButton.Location
+                = new Point(ClientSize.Width - menuToSettingsButton.Width, 0);
+        }
+
+        //this section is some code that will run whenever the size of the form is changed
+        private void MainWindow_SizeChanged(object sender, EventArgs e)
+        {
+            //the following code keeps all the panels to be the same size as the form
+            //ClientSize is the size of the form MainWindow
+            menuPage.Size = new Size(ClientSize.Width, ClientSize.Height);
+            previewAndSendDataPage.Size = new Size(ClientSize.Width, ClientSize.Height);
+            settingsPage.Size = new Size(ClientSize.Width, ClientSize.Height);
+
+            //the following code keeps the locations of buttons and other controls where they were
+            //relative to where they started
+            menuToPreviewButton.Location
+                = new Point(ClientSize.Width / 2 - menuToPreviewButton.Width / 2, ClientSize.Height / 2);
+            menuToSettingsButton.Location
+                = new Point(ClientSize.Width - menuToSettingsButton.Width, 0);
+        }
+
+        //this section is code that runs when menuToPreviewButtonIsClicked
+        private void menuToPreviewButton_Click(object sender, EventArgs e)
+        {
+            //the following switches the visible panel on the form from menu to preview page
+            menuPage.Visible = false;
+            previewAndSendDataPage.Visible = true;
+        }
+
+        //this section is code that runs when menuToSettingsButton is cliccked
+        private void menuToSettingsButton_Click(object sender, EventArgs e)
+        {
+            //the following switches the visible panel on the form from menu to settings page
+            menuPage.Visible = false;
+            settingsPage.Visible = true;
         }
     }
 }
