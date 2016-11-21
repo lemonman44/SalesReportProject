975e6c242d8498aed795bbe164740c4acb645eab
Brendon Brown <bbrown16@northlandcaps.org>
2016-11-21 14:25:39 -0600

    added picture

--- SalesReportProject/SalesReportProject/Properties/Resources.Designer.cs
+++ SalesReportProject/SalesReportProject/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace SalesReportProject.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap l {
+            get {
+                object obj = ResourceManager.GetObject("l", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap SOR_logo {
             get {
                 object obj = ResourceManager.GetObject("SOR-logo", resourceCulture);
